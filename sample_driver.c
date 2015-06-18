b99b6dfa58716914e9f4db3c35c08d6ce0ca7960
yakihiro@ac.auone-net.jp <yakihiro@ac.auone-net.jp>
2015-06-19 02:05:56 +0900

    Add printk

--- sample_driver.c
+++ sample_driver.c
@@ -5,11 +5,13 @@ MODULE_LICENSE("Dual MIT/GPL");
 
 static int symple_driver_init(void)
 {
+	printk(KERN_ALERT "driver loaded\n");
 	return 0;
 }
 
 static void simple_driver_exit(void)
 {
+	printk(KERN_ALERT "driver unloaded\n");
 }
 
 module_init(symple_driver_init);
